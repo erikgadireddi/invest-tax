0f05ee5cfb12b8c4b0b61f0e1cab16bf14aaa5c2
DeirhX <deirh.mana@gmail.com>
2025-02-05 00:08:43 +0100

    Fixed the split algorithm + support positions that have a different index

--- matchmaker/trade.py
+++ matchmaker/trade.py
@@ -70,20 +70,21 @@ def add_split_data(target: pd.DataFrame, split_actions: pd.DataFrame) -> pd.Data
     split_actions = split_actions[split_actions['Action'] == 'Split']
     if not split_actions.empty:
         # Sort split actions by date and compute cumulative ratio
-        split_actions = split_actions.sort_values(by='Date/Time', ascending=True)
+        split_actions = split_actions.sort_values(by='Date/Time', ascending=False)
         split_actions['Cumulative Ratio'] = split_actions.groupby('Symbol')['Ratio'].cumprod()
 
         # Merge target with split_actions to align split ratios with trades. We'll have many-to-one mapping here, to be cleaned later.
         merged = target.reset_index().merge(split_actions, on='Symbol', suffixes=('', '_split'), how='left')
+        index_col = 'Hash' if 'Hash' in merged.columns else 'index' 
 
-        # Filter out rows where the split action date is not greater than the trade date
-        filtered = merged[merged['Date/Time_split'] <= merged['Date/Time']]
+        # Filter out rows where the split action date is in the future relative to the trade date
+        filtered = merged[merged['Date/Time_split'] >= merged['Date/Time']]
 
-        # Create a binding that can be used to look up the correct split ratio (latest date) for each trade
-        max_date_split = filtered.groupby('Hash')['Date/Time_split'].idxmax()
+        # Create a binding that can be used to look up the correct split ratio (one with the latest date) for each trade
+        split_lookup = filtered.groupby(index_col)['Date/Time_split'].idxmin()
         
-        # Apply the lookup to find the cumulative ratio
-        target.loc[max_date_split.index, 'Split Ratio'] = filtered.loc[max_date_split, 'Cumulative Ratio'].values
+        # Apply the lookup to bind to the cumulative ratio
+        target.loc[split_lookup.index, 'Split Ratio'] = 1 / filtered.loc[split_lookup, 'Cumulative Ratio'].values
 
         # Fill NaN values with 1 (for trades with no applicable split actions)
         target['Split Ratio'].fillna(1.0, inplace=True)
